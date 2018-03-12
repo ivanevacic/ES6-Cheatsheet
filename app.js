30612a645b4470753540f4423137485f73c3bc80
ivanevacic <ivanevacicweb@gmail.com>
2018-03-12 18:10:58 +0100

    FInished Template Literals section

--- app.js
+++ app.js
@@ -211,5 +211,34 @@ val = firstName[0];	//access 1st letter in string
 		val = str.includes('Hello');//	if there is occurance of 'Hello' in string,return true,else false
 console.log(val); */
 
+//	TEMPLATE LITERALS
+const name = 'John';
+const age = 30;
+const job = 'Web Developer';
+const city = 'London';
+let html;
+	//without template strings
+	html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + ' </li></li>Job: ' + job + ' </li><li>City: ' + city + '</li></ul>';
+
+		//	with template strings
+			//Alt + 7
+
+		function hello() {
+			return 'hello';
+		}
+
+		html = `
+		<ul>
+			<li>Name: ${name}</li>
+			<li>Age: ${age}</li>
+			<li>Job: ${job}</li>
+			<li>City: ${city}</li>
+			<li>${2 + 2}</li>
+			<li>${hello()}</li>
+			<li>${age >= 30 ? 'Over' : 'Under 30'}</li>
+		</ul>
+		`;
+	//append let html value to index.html dynamically
+	document.body.innerHTML = html;
 
 
