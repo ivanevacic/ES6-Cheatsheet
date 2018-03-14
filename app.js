bfa7d1a78337d588c0874459e0361deb3e9e6779
ivanevacic <ivanevacicweb@gmail.com>
2018-03-14 01:30:46 +0100

    Finished Object.create section

--- app.js
+++ app.js
@@ -776,7 +776,7 @@ john.getsMarried('Test');
 console.log(john.getFullName());	//john test
 
 
-*/
+
 
 
 //	PROTOTYPAL INHERITANCE
@@ -816,3 +816,35 @@ Customer.prototype.greeting = function(){
 	return `Hello there ${this.firstName} ${this.lastName},Welcome to our Company`;
 }
 console.log(customer1.greeting());//Hello there Tom Smith,Welcome to our Company
+*/
+
+//OBJECT.CREATE
+
+const personPrototypes = {
+	greeting: function(){
+		return `Hello there ${this.firstName} ${this.lastName}`;
+	},
+	getsMarried: function(newLastName){
+		this.lastName = newLastName;
+	}
+}
+
+const mary = Object.create(personPrototypes);//it will take in prototypes
+//add properties
+mary.firstName = 'Mary';
+mary.lastName = 'Williams';
+mary.age = 30;
+
+console.log(mary);//{firstName: "Mary", lastName: "Williams", age: 30}
+mary.getsMarried('Thompson');
+console.log(mary.greeting());//Hello there Mary Thompson
+
+//	alternative syntax for adding properties
+const brad = Object.create(personPrototypes, {
+	firstName: {value: 'Brad'},
+	lastName: {value:'Zukov'},
+	age: {value: 30}
+});
+
+console.log(brad);//{firstName: "Brad", lastName: "Zukov", age: 30}
+console.log(brad.greeting());//Hello there Brad Zukov
