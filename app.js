1ec20552aa4650413bef8db075fd7ae0365e6849
ivanevacic <ivanevacicweb@gmail.com>
2018-03-20 18:54:44 +0100

    Finished Maps section

--- app.js
+++ app.js
@@ -1127,4 +1127,85 @@ console.log(name, age, city, sayHello);//John Doe 32 Miami
 	//console.log('Hello');
 //}
 
+
+
+//MAPS
+	//new object structure introduced in es6
+		//hold key:value pars
+			//any TYPE can be value or key
+
+const map1 = new Map();
+//set keys
+const key1 = 'some string',
+	  key2 = {},
+	  key3 = function(){};
+
+//set map values by key
+
+map1.set(key1, 'Value of key1');
+map1.set(key2, 'Value of key2');
+map1.set(key3, 'Value of key3');
+
+//get values by key
+//console.log(map1.get(key1));//Value of key1
+///console.log(map1.get(key2));//Value of key2
+//console.log(map1.get(key3));//Value of key3
+
+
+//count values
+//console.log(map1.size);//3->3 key value pars inside map
+
+//iterating trough maps
+	//loop using for...of to get keys and values
+		for(let [key, value] of map1){
+			console.log(`${key} = ${value}`);
+		}
+		//some string = Value of key1
+		//[object Object] = Value of key2
+		//function (){} = Value of key3
+
+		//iterate keys only
+		for(let key of map1.keys()) {
+			console.log(key);
+		}
+		//some string
+		//{}
+		//ƒ (){}
+
+		//iterate values only
+		for(let value of map1.values()) {
+			console.log(value);
+		}
+		//Value of key1
+		//Value of key2
+		//Value of key3
+
+	//loop using forEach
+	map1.forEach(function(value, key) {
+		console.log(`${key} = ${value}`);
+	});
+	//some string = Value of key1
+	//[object Object] = Value of key2
+	//function (){} = Value of key3
+
+
+//CONVERT TO ARRAYS
+
+	//CREATE ARRAY OF KEY-VALUE PAIRS
+
+	const keyValArr = Array.from(map1);
+	console.log(keyValArr);
+	//(3) [Array(2), Array(2), Array(2)]
+
+	//create an array of values
+	const valArr = Array.from(map1.values());
+	console.log(valArr);
+	//(3) ["Value of key1", "Value of key2", "Value of key3"]
+
+	//create an array of keys
+	const keyArr = Array.from(map1.keys());
+	console.log(keyArr);
+	//(3) ["some string", {…}, ƒ]
+
 */
+
